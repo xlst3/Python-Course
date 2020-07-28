9e8e43095836c38847e162b60e7c0df94de41fc4
binarflow <clouds.overflow@gmail.com>
2020-07-28 13:38:02 -0400

    template startup

---
+++ 1-8.25/types.py
@@ -0,0 +1,68 @@
+# Text Type:	    str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	    dict
+# Set Types:	    set, frozenset
+# Boolean Type:	    bool
+# Binary Types:	    bytes, bytearray, memoryview
+
+# str, int, float, bool, list
+
+# str
+"this is a string"
+
+# int
+125
+
+#float
+3.14
+
+# bool
+True, False
+
+# list
+[4, True, "hi", 9.8, False, 10] # types of values seperated by
+                                # comma inside a bracket
+
+# ----------------------------------------------------------- #
+
+# variables
+x = 5
+# a variable can be named whatever you want, in this case
+# I created a variable called X and gave it a value of 5
+
+# examples
+age = 17
+
+happy = True
+
+thingsToBuy = ["sausage", "chicken", "soap", "cookies"]
+
+pi = 3.1415926
+
+# things to watch out when creating variables
+
+# 1. case sensitive
+a = 1
+A = 1
+
+# 2. no space
+my name = "Richard"
+
+# 3. can't use python keyword
+is = 3
+and = 6
+print = 10
+
+# 4. No quotes and operators
+tianyi's dad = "Richard"
+he-llo = 'hello'
+
+# Conclusion: just use letters for variables
+# when spacing is needed just do
+todayIsaNiceDay = True
+today_is_a_nice_day = True
+
+
+
+
